ff05fd953e281e1f659837510a46b8013bc4c06e
razekmh <razekmh@gmail.com>
2021-10-07 02:32:44 +0100

    without couch

--- install_arches.sh
+++ install_arches.sh
@@ -2,11 +2,26 @@
 sudo apt-get update -y
 sudo apt-get full-upgrade -y 
 
-sudo apt-get install -y make software-properties-common build-essential libxml2-dev libproj-dev libjson-c-dev xsltproc docbook-xsl docbook-mathml libgdal-dev libpq-dev python3-venv
+sudo add-apt-repository "deb http://archive.ubuntu.com/ubuntu $(lsb_release -sc) universe"
+sudo apt-get update -y
+sudo apt-get install -y make software-properties-common build-essential libxml2-dev
+  #sudo add-apt-repository -y ppa:ubuntugis/ubuntugis-unstable
+
+sudo apt-get install -y build-essential
+sudo apt-get install -y libxml2-dev
+sudo apt-get install -y libproj-dev
+sudo apt-get install -y libjson-c-dev
+sudo apt-get install -y xsltproc
+sudo apt-get install -y docbook-xsl
+sudo apt-get install -y docbook-mathml
+sudo apt-get install -y libgdal-dev
+sudo apt-get install -y libpq-dev
 
 # install_postgres
-sudo add-apt-repository "deb http://apt.postgresql.org/pub/repos/apt/ $(lsb_release -sc)-pgdg main"
-wget --quiet -O - http://apt.postgresql.org/pub/repos/apt/ACCC4CF8.asc | sudo apt-key add -
+sudo apt update -y
+sudo apt -y install gnupg2
+wget --quiet -O - https://www.postgresql.org/media/keys/ACCC4CF8.asc | sudo apt-key add -
+echo "deb http://apt.postgresql.org/pub/repos/apt/ `lsb_release -cs`-pgdg main" |sudo tee  /etc/apt/sources.list.d/pgdg.list
 sudo apt-get update -y
 sudo apt-get install postgresql-12 postgresql-contrib-12 -y
 sudo apt-get install postgresql-12-postgis-3 -y
@@ -32,12 +47,37 @@ sudo -u postgres psql -d template_postgis -c "GRANT ALL ON geometry_columns TO P
 sudo -u postgres psql -d template_postgis -c "GRANT ALL ON geography_columns TO PUBLIC;"
 sudo -u postgres psql -d template_postgis -c "GRANT ALL ON spatial_ref_sys TO PUBLIC;"
 
-# install couchdb
-sudo apt update && sudo apt install -y curl apt-transport-https gnupg
-curl https://couchdb.apache.org/repo/keys.asc | gpg --dearmor | sudo tee /usr/share/keyrings/couchdb-archive-keyring.gpg >/dev/null 2>&1
-source /etc/os-release
-echo "deb [signed-by=/usr/share/keyrings/couchdb-archive-keyring.gpg] https://apache.jfrog.io/artifactory/couchdb-deb/ ${VERSION_CODENAME} main" \
-	    | sudo tee /etc/apt/sources.list.d/couchdb.list >/dev/null
-sudo apt-get update
-sudo apt-get install -y couchdb
+
+# install yarn
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update -y
+sudo apt-get install yarn -y
+
+
+# install elasticsearch
+sudo apt-get install openjdk-8-jre-headless -y
+sudo apt-get install apt-transport-https -y
+wget -qO - https://artifacts.elastic.co/GPG-KEY-elasticsearch | sudo apt-key add -
+sudo sh -c 'echo "deb https://artifacts.elastic.co/packages/7.x/apt stable main" > /etc/apt/sources.list.d/elastic-7.x.list'
+sudo apt-get update -y
+sudo apt-get install elasticsearch -y 
+sudo /bin/systemctl enable elasticsearch.service
+sudo systemctl enable elasticsearch.service
+sudo systemctl start elasticsearch.service
+
+# create project
+cd /opt
+mkdir projects && cd projects
+sudo apt-get install python3-dev python3-venv -y
+python3 -m venv ENV
+source ENV/bin/activate
+git clone -b stable/5.2.x https://github.com/archesproject/arches.git
+cd arches
+pip install -e . 
+pip install -r arches/install/requirements_dev.txt 
+cd ..
+arches-project create my_project
+cd my_project
+python manage.py setup_db
 
